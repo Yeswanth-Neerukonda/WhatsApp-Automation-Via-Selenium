d2c1b3a89ef5299deed1323ac2df816b3938cccf
Yeswanth Neerukonda <77625058+Yeswanth-Neerukonda@users.noreply.github.com>
2025-08-14 13:10:23 +0100

    Update WhatsApp_Code.py

--- WhatsApp_Code.py
+++ WhatsApp_Code.py
@@ -1,29 +1,3 @@
-import pywhatkit
-# Phone_Number = "+44 7553 409888"
-# Message = "Hello Rahul"
-# time_hour = 1
-# time_minute = 47
-# time_out = 30
-# tab_close = True
-# close_time = 59
-# pywhatkit.sendwhatmsg(Phone_Number, Message, time_hour, time_minute, time_out, tab_close, close_time)
-
-
-
-# contacts = [
-#     ("+447884826027", "Hello Alice! How are you?"),
-#     ("+447760513276", "Hi Bob! Don't forget our meeting."),
-#     ("+447553409888", "Hey Carol! Have a great day! 🌸")
-# ]
-
-# # Time to send messages (24-hour format)
-# hour = 2
-# minute = 23
-
-# for number, message in contacts:
-#     pywhatkit.sendwhatmsg(number, message, hour, minute, wait_time=30, tab_close=True, close_time=59)
-
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
@@ -71,21 +45,11 @@ for number, message in contacts:
             inputBox.dispatchEvent(e);
         }
         """)
-    # try:
-    #     # Wait until the green send button is clickable
-    #     send_button = WebDriverWait(driver, 30).until(
-    #         EC.element_to_be_clickable((By.XPATH, '//span[@data-icon="Message"]'))
-    #     )
-
-    #     # Wait for send button and click it
-    #     send_button = WebDriverWait(driver, 30).until(
-    #         EC.element_to_be_clickable((By.XPATH, '//button[@aria-label="Send"]'))
-    #     )
-        # send_button.click()
         print(f"✅ Message sent to {number}")
     except:
         print(f"❌ Failed to send message to {number}")
     time.sleep(15)
 
 print("All messages sent successfully!")
+
 driver.quit()
